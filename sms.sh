a64d5de9ed97e99a2db571f81657be4f5283db5c
riskyprsty <66415817+riskyprsty@users.noreply.github.com>
2020-06-04 18:09:55 +0700

    Add files via upload

---
+++ sms.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#nub gweh bang
+#script cuma echo doang
+echo "
+
+  _____ __  __  _____ ______             
+ / ____|  \/  |/ ____|  ____|            
+| (___ | \  / | (___ | |__ _ __ ___  ___ 
+ \___ \| |\/| |\___ \|  __| '__/ _ \/ _ \
+ ____) | |  | |____) | |  | | |  __/  __/
+|_____/|_|  |_|_____/|_|  |_|  \___|\___|
+                                         
+                                         
+"
+echo -e "\e[1;31m SCRIPT BY PRSTY \e[0m"
+echo -e "\e[1;31m • PASTIKAN TERINSTAL PLUGIN TERMUX:API \e[0m"
+echo -e "\e[1;42m • KHUSUS TELKOMSEL \e[0m"
+echo -e "\e[1;42m • PESAN DARURAT UTAMAKAN SINGKAT \e[0m"
+date
+echo
+echo -ne '#####                     (33%)\r'
+sleep 1
+echo -ne '#############             (66%)\r'
+sleep 1
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
+echo "Masukkan Nomor Hp:"
+read nomor
+echo "Masukkan Pesan :"
+read pesan
+termux-sms-send -n $nomor $pesan+😔
+echo "Bentar Bro Proses"
+echo -ne '#####                     (33%)\r'
+sleep 1
+echo -ne '#############             (66%)\r'
+sleep 1
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
+echo "Blok nomer doi dulu yahahah"
+echo
+echo
+echo "Done"
+echo "~~ Kalau gagal, send manual~~"
+
